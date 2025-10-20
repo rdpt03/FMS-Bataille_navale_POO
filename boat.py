ecb779322f8130bdbd041f49309e75a5c5d86e7f
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-10-20 13:24:10 +0200

    added boat and boat_cases and it's attributes to each one

---
+++ boat.py
@@ -0,0 +1,4 @@
+class Boat:
+    def __init__(self, name : str, cases : list):
+        self.name = name
+        self.cases = cases
---
+++ boat_case.py
@@ -0,0 +1,4 @@
+class BoatCase:
+    def __init__(self,case_nb : str):
+        self.case_nb = case_nb
+        self.hit = False
