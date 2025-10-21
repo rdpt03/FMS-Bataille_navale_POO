4117eeec868381b0b4f96f664db5859b0e12ca9a
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-10-21 09:08:46 +0200

    add : function in boat_case to check if this BoatCase is used on a given list

--- boat_case.py
+++ boat_case.py
@@ -2,3 +2,9 @@ class BoatCase:
     def __init__(self,case_nb : str):
         self.case_nb = case_nb
         self.hit = False
+
+    def in_list(self, boat_case_list : list):
+        for e in boat_case_list:
+            if e.case_nb == self.case_nb:
+                return True
+        return False
